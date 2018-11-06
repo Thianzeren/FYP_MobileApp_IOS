fe3d49218f3002a3de2f0f82eee72fbda4c50148
Nicholas <nicholas@thaos-macbook-air.local>
2018-11-07 03:11:03 +0800

    Completed LoginNameController, saved responses to instanceDAO

--- Engagingu/Controller/LoginNameController.swift
+++ Engagingu/Controller/LoginNameController.swift
@@ -22,10 +22,62 @@ class LoginNameController: UIViewController, UITextFieldDelegate {
             return
         }
         
+        //Send username to db via POST JSON
+        let json: [String: String] = ["username": username]
+        print(username)
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: json) else { return
+            print("Error: cannot create jsonData")
+        }
+        let string1 = String(data: jsonData, encoding: String.Encoding.utf8) ?? "Data could not be printed"
+        print(string1)
+        httpPost(jsonData: jsonData)
+        
         performSegue(withIdentifier: "toHomeSegue", sender: nil)
         
     }
     
+    func httpPost(jsonData: Data){
+        if !jsonData.isEmpty {
+            
+            guard let url = URL(string: "http://54.255.245.23:3000/user/register") else {
+                
+                print("Error: cannot create URL")
+                return
+            }
+            var request = URLRequest(url: url)
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpMethod = "POST"
+            request.httpBody = jsonData
+            
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                guard let data = data, error == nil else{
+                    print(error?.localizedDescription ?? "No data")
+                    return
+                }
+                
+                do{
+                    guard let responseJSON = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else {
+                        print("Error: no json response received")
+                        return
+                    }
+                    
+                    print(responseJSON)
+                    
+                    let team_id = responseJSON["team_id"] as? Int
+                    print(team_id)
+                    InstanceDAO.team_id = String(team_id!)
+                    print(InstanceDAO.team_id)
+                    
+                }catch let jsonErr{
+                    print ("Error serializing json:" + jsonErr.localizedDescription)
+                }
+                
+               
+                
+            }
+            task.resume();
+        }
+    }
     func createAlert (title:String, message:String){
         let alert = UIAlertController(title: title, message:message, preferredStyle: UIAlertController.Style.alert)
         
