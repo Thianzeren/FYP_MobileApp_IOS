65ef8484ebef7faba6219b727168016558f802f0
Raylene <raylene.ng.2016@sis.smu.edu.sg>
2019-01-15 12:21:19 +0800

    Celine: -Drawing game

---
+++ Engagingu/Controller/DrawingController.swift
@@ -0,0 +1,81 @@
+//
+//  DrawingController.swift
+//  Engagingu
+//
+//  Created by Raylene on 15/1/19.
+//  Copyright © 2019 Raylene. All rights reserved.
+//
+
+import UIKit
+
+class DrawingController: UIViewController {
+
+    @IBOutlet weak var drawing: UIImageView!
+    var lastPoint = CGPoint.zero
+    var color = UIColor.black
+    var brushWidth: CGFloat = 10.0
+    var opacity: CGFloat = 1.0
+    var swiped = false
+    
+    var lastTouch = CGPoint.zero
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        swiped = false
+        lastPoint = touch.location(in: view)
+    }
+    func drawLine(from fromPoint: CGPoint, to toPoint: CGPoint) {
+        // 1
+        UIGraphicsBeginImageContext(view.frame.size)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        drawing.image?.draw(in: view.bounds)
+        
+        // 2
+        context.move(to: fromPoint)
+        context.addLine(to: toPoint)
+        
+        // 3
+        context.setLineCap(.round)
+        context.setBlendMode(.normal)
+        context.setLineWidth(brushWidth)
+        context.setStrokeColor(color.cgColor)
+        
+        // 4
+        context.strokePath()
+        
+        // 5
+        drawing.image = UIGraphicsGetImageFromCurrentImageContext()
+        drawing.alpha = opacity
+        UIGraphicsEndImageContext()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        
+        // 6
+        swiped = true
+        let currentPoint = touch.location(in: view)
+        drawLine(from: lastPoint, to: currentPoint)
+        
+        // 7
+        lastPoint = currentPoint
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !swiped {
+            // draw a single point
+            drawLine(from: lastPoint, to: lastPoint)
+        }
+    }
+    
+}
+
