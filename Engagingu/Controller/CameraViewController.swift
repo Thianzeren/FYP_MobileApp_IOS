f7bbd7ea3135de3fff35b4cb6bb76a57a1812cf9
Nicholas <nicholas@thaos-macbook-air.local>
2019-01-16 20:07:06 +0800

    Fixed the slow start on the imagepickercontroller (when press take photo to select camera)

--- Engagingu/Controller/CameraViewController.swift
+++ Engagingu/Controller/CameraViewController.swift
@@ -15,10 +15,14 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     var question: String = ""
     var hotspot: String = ""
     var image: UIImage?
+    var imagePickerController: UIImagePickerController?
     
     override func viewDidLoad() {
         question = InstanceDAO.selfieDict[hotspot] ?? ""
         selfieQuestion.text = question
+        
+        imagePickerController = UIImagePickerController()
+        imagePickerController!.delegate = self
         
         super.viewDidLoad()
          // Do any additional setup after loading the view.
@@ -27,16 +31,13 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     @IBAction func chooseImage(_ sender: Any) {
         
-        let imagePickerController = UIImagePickerController()
-        imagePickerController.delegate = self
-        
         let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a Source", preferredStyle: .actionSheet)
         
         actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action:UIAlertAction) in
             
             if(UIImagePickerController.isSourceTypeAvailable(.camera)){
-                imagePickerController.sourceType = .camera
-                self.present(imagePickerController, animated: true, completion: nil)
+                self.imagePickerController!.sourceType = .camera
+                self.present(self.imagePickerController!, animated: true, completion: nil)
             }else{
                 print("Camera not available")
             }
@@ -45,8 +46,8 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         
         actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {(action:UIAlertAction) in
             
-            imagePickerController.sourceType = .photoLibrary
-            self.present(imagePickerController, animated: true, completion: nil)
+            self.imagePickerController!.sourceType = .photoLibrary
+            self.present(self.imagePickerController!, animated: true, completion: nil)
             
         }))
         
